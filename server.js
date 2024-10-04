347190c048b49527d99729b6ca344d88977e434e
unknown <jayaphwn@gmail.com>
2024-10-04 10:51:07 +0700

    update

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const db_connect = require("./config/dbConnection");
 const userRouter = require("./router/userRouter");
 const errorHandle = require("./middleware/errorHandler");
 const noteRouter = require("./router/noteRouter");
+const cors = require("cors");
+const cors = require("cors");
 const dotenv = require("dotenv").config();
 const app = express();
 
@@ -10,6 +12,7 @@ db_connect();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use("/api/users", userRouter);
 app.use("/api/notes", noteRouter);
 app.get("/", (req, res) => res.send("Welcome to My World,, stay safe :)!"));
